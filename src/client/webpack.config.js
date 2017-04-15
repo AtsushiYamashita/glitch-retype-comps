06f69170911e57f2f1618e3af6537b4789c7b902
System <none>
2017-04-15 22:16:06 +0000

    Checkpoint

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./src/client/app.tsx",
     output: {
         filename: "app.js",
-        path: __dirname + "/public"
+        path: "/app/public"
     },
 
     // Enable sourcemaps for debugging webpack's output.
@@ -39,8 +39,8 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    //externals: {
-        //"react": "React",
-        //"react-dom": "ReactDOM"
-    //},
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
 };
