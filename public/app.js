6839f028b108d484ba632997245d8ea285d565f0
System <none>
2017-04-15 21:05:39 +0000

    Checkpoint

---
+++ public/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactDOM = require("react-dom");
+var hello_1 = require("./hello");
+ReactDOM.render(React.createElement(hello_1.Hello, { compiler: "TypeScript", framework: "React" }), document.getElementById("example"));
+//# sourceMappingURL=app.js.map
---
+++ src/client/hello.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react'
+
+export const Hello = (props) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
